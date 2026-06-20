44c6c1babedf9e1902ea088f2322a050fae3d8fe
Ousep003 <josephdominic0032@gmail.com>
2026-06-20 10:00:00 +0530

    20-06-2026 DataTypes.java - Demonstrate primitive data types and sizes

---
+++ CO1/DataTypes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = 100;
+        System.out.println("Integer Value: " + num);
+
+        long num1 = 1000000;
+        System.out.println("Long Value: " + num1);
+
+        float num2 = 25.5f;
+        System.out.println("Float Value: " + num2);
+
+        double num3 = 123.456d;
+        System.out.println("Double Value: " + num3);
+
+        char myGrade = 'B';
+        System.out.println("Character:" + myGrade);
+
+        boolean bool = true;
+        System.out.println("Boolean:" + bool);
+
+
+    }
+}
