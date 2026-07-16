1c9ab976cc4294857aed06407adbbc5efb71c657
Ousep003 <josephdominic0032@gmail.com>
2026-07-16 15:30:00 +0530

    16-07-2026 Simple_Calculator.java - Basic arithmetic calculator

---
+++ untitled/Simple_Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Simple_Calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers");
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+
+        System.out.println("Addition: " + (number1 + number2));
+        System.out.println("Difference: " + (number1 - number2));
+        System.out.println("Product: " + (number1 * number2));
+        System.out.println("Quotient: " + (number1 / number2));
+        System.out.println("Modulus: " + (number1 % number2));
+    }
+
+}
