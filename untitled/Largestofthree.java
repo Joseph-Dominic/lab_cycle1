74b217d14ff0135733aee6590ff6e9320323a2d0
Ousep003 <josephdominic0032@gmail.com>
2026-07-20 13:40:00 +0530

    20-07-2026 Largestofthree.java - Find largest among three numbers

---
+++ untitled/Largestofthree.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Largestofthree{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three numbers:");
+        int numb1 = sc.nextInt();
+        int numb2 = sc.nextInt();
+        int numb3 = sc.nextInt();
+
+        if (numb2 > numb1 && numb2 > numb3) {
+            System.out.print(numb2 + " is Largest");
+        }
+        else if (numb3 > numb1 && numb3 > numb2){
+                System.out.println(numb3 + " is largest");
+            }else {
+                System.out.println(numb1 + " is largest");
+            }
+    }
+}
