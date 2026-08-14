6e38798fe0d20727f46fe435126b014bdebe66e5
Ousep003 <josephdominic0032@gmail.com>
2026-08-14 09:45:00 +0530

    14-08-2026 Main.java - Person and student display program

---
+++ CO3/Main.java
@@ -0,0 +1,14 @@
+class Person{
+    String name;
+    Person(String name){
+        this.name = name;
+    }
+    void displayName(){
+        System.out.println(name);
+    }
+}
+
+class Student{
+    String name;
+
+}
