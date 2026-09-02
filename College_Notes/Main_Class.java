4383addf3c15bfa1e1e522cc7e62c8ecbc0b6f5d
Ousep003 <josephdominic0032@gmail.com>
2026-09-02 11:30:00 +0530

    02-09-2026 Main_Class.java - Box class and dimension variables

---
+++ College_Notes/Main_Class.java
@@ -0,0 +1,12 @@
+class Box{
+    int length;
+    int breadth;
+    int height;
+    static int boxCount;
+
+    Box(int length, int breadth, int height){
+        this.lenght = length
+        this.breadth = breadth
+        this.
+}
+}
